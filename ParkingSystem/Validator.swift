6f62b8823204a9a4e1750e8083101eb81340ff72
Yilei Pan <robert-pan@hotmail.com>
2018-11-02 21:33:20 +1100

    Validator

---
+++ ParkingSystem/Validator.swift
@@ -0,0 +1,13 @@
+//
+//  Validator.swift
+//  ParkingSystem
+//
+//  Created by Yilei Pan on 2/11/18.
+//  Copyright © 2018 Yilei.Pan. All rights reserved.
+//
+
+import Foundation
+
+class Validator {
+    
+}
